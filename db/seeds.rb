d70f43f7da4671da9a5b0d57d1dc02a6c1cf4c98
reevesalic <reevesalic@gmail.com>
2021-01-28 16:47:10 -0500

    category model and migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+finacial = Category.create(name: "Financial")
+health = Category.create(name: "Health")
+misc = Category.create(name: "Misc")
